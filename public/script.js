efe3bbc71ec1b1fc994d565a8d1ba1c7f40b5c28
ckheav@terpmail.umd.edu <52798523+ckheav@users.noreply.github.com>
2021-03-06 12:56:34 -0500

    JavaScript changes after malfunction

--- public/script.js
+++ public/script.js
@@ -1,27 +1,50 @@
-const endpoint = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
-console.log(endpoint);
-const restaurants_name = [];
+async function windowActions(){
+  const endpoint = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
+  console.log(endpoint);
+  const restaurants_name = [];
 
-fetch(endpoint)
-  .then((blob) => blob.json())
-  .then((data) => restaurants_name.push(...data));
-console.log(restaurants_name);
+  const request = await fetch(endpoint)
+    .then((blob) => blob.json())
+    .then((data) => restaurants_name.push(...data));
+  console.log(restaurants_name);
 
-let userInput = document.querySelector("#userInput");
-let container = document.querySelector(".container");
+  let userInput = document.querySelector("#userInput");
+  let container = document.querySelector(".container");
 
-userInput.addEventListener("keyup", () => {
-  let query = userInput.value;
-  let string = [];
-  string.push(query);
-  console.log(string);
-  console.log(findMatches(query, restaurants_name));
-});
-
-function findMatches(wordToMatch, restaurants) {
-  return restaurants.filter((name) => {
-    const regex = new RegExp(wordToMatch, "gi");
-    return name.name.match(regex) || name.zip.match(regex);
+  userInput.addEventListener("keyup", () => {
+    let query = userInput.value;
+    let string = [];
+    string.push(query);
+    console.log(string);
+    console.log(findMatches(query, restaurants_name));
   });
+
+  function findMatches(wordToMatch, restaurants) {
+    return restaurants.filter((name) => {
+      const regex = new RegExp(wordToMatch, "gi");
+      return name.name.match(regex) || name.zip.match(regex) || name.category.match(regex);
+    });
+  }
+
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, restaurants_name);
+    const html = matchArray.map(name => {
+      return `
+        <li>
+          <span class="name">${name.name}, ${name.city}, ${name.zip}</span>
+        </li>
+      `;
+    });
+
+    suggestions.innerHTML = html;
+  }
+
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
+
+  searchInput.addEventListener("change", displayMatches);
+  searchInput.addEventListener("keyup", (evt) => {displayMatches(evt)});
+
 }
+window.onload = windowActions;
 
