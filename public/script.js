4f7612dff76fccd547066e3fa35e0059fce66514
jhg <jbbillion@yahoo.com>
2021-03-06 00:51:26 -0500

    finished

--- public/script.js
+++ public/script.js
@@ -28,21 +28,23 @@ function findMatches(wordToMatch, restaurants) {
 
 function displayMatches() {
   const matchArray = findMatches(this.value, restaurants_name);
-  const html = matchArray
-    .map((name) => {
-      return `
+  const html = matchArray.map(name => {
+    return `
       <li>
-        <span class="name">${name.name}, ${name.zip}</span>
-        span.
-        </li>
+        <span class="name">${name.name}, ${name.city}, ${name.zip}</span>
+      </li>
     `;
-    })
-    .join("");
+  });
+
   suggestions.innerHTML = html;
 }
 
-const searchInput = document.querySelector(".search");
-const suggestions = document.querySelector(".suggestions");
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
 
 searchInput.addEventListener("change", displayMatches);
 searchInput.addEventListener("keyup", displayMatches);
+
+
+
+
